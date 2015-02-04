802380c13a17296cbfe8c2f8f473b91cb575ddd5
Makers Laptop 88 <makerslaptop88@Makerss-MacBook-Pro-4.local>
2015-02-04 16:09:21 +0000

    docking station test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,17 +1,21 @@
 require 'docking_station.rb'
-require 'bike.rb'
+#require 'bike.rb'
 
 describe DockingStation do
 
 	def fill_station(station)
-		20.times { station.dock(Bike.new) }
+		20.times { station.dock(bike) }
 	end
 	
-	let(:bike) { Bike.new }
+	#let(:bike) { Bike.new }
+	let(:bike) {double :bike, fix!: false}#you can add in methods and return values to build a bike
+	#e.g. 
+	#let(:bike) {double :bike, working? : true, fix!: }
 	let(:station) { DockingStation.new(capacity: 20)}
 
+
 	it 'should accept a bike' do
-		bike = Bike.new
+		#bike = Bike.new
 		station = DockingStation.new
 		expect(station.bike_count).to eq(0)
 		station.dock(bike)
@@ -33,6 +37,11 @@ describe DockingStation do
 	it 'should not accept a bike if it\'s full' do
 		fill_station(station)
 		expect(lambda { station.dock(bike) }).to raise_error(RuntimeError, 'Station is full')
+	end
+
+	it 'should fix itself' do
+		station.dock(bike)
+		expect(bike.fix!).to eq true#here this takes in the method defines in double.
 	end
 
 end
