098be77d4465218b171e3cc066d5237f21e35a77
Makers Laptop 88 <makerslaptop88@Makerss-MacBook-Pro-4.local>
2015-02-04 17:39:58 +0000

    completed boris bikes now on to exercises

--- spec/bike_container_spec.rb
+++ spec/bike_container_spec.rb
@@ -1,16 +1,50 @@
 require './lib/bike_container'
 
-class ContainerHolder; include BikeContainer; end
+class ContainerHolder
+	include BikeContainer
+ end
 
 describe BikeContainer do 
 
+	def fill_station(station)
+		20.times { station.dock(bike) }
+	end
+
 	let(:bike) { double :bike }
 	let(:holder) { ContainerHolder.new }
+	let(:working_bike) { double :bike, broken?: false}
+	let(:broken_bike) { double :bike, broken?: true}
 
 	it 'should accept a bike' do
 		expect(holder.bike_count).to eq(0)
 		holder.dock(bike)
 		expect(holder.bike_count).to eq(1)
 	end
+
+
+	it 'should release a bike' do
+		holder.dock(bike)
+		holder.release(bike)
+		expect(holder.bike_count).to eq(0)
+	end
+
+	it 'should know when it\'s full' do
+		expect(holder).not_to be_full
+		fill_station(holder)
+		expect(holder).to be_full
+	end
+
+	it 'should not accept a bike if it\'s full' do
+		fill_station(holder)
+		expect(lambda { holder.dock(bike) }).to raise_error(RuntimeError, 'Station is full')
+	end
+
+	it 'should provide the list of available bikes' do
+		holder.dock(working_bike)
+		holder.dock(broken_bike)
+		expect(holder.available_bikes).to eq([working_bike])
+
+	end
+
 	
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,11 +3,12 @@ require 'docking_station.rb'
 
 describe DockingStation do
 
-
 	let(:station) { DockingStation.new(capacity: 123)}
 
 	it 'should allow setting default capacity on initialising' do
 		expect(station.capacity).to eq(123)
+
 	end
+
 
 end
